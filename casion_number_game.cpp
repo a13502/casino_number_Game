418b66357cee699b3fdf848ee1fa6f34b489bc39
Aneri patel <92584421+a13502@users.noreply.github.com>
2024-09-03 11:51:13 +0530

    Update casion_number_game.cpp

--- casion_number_game.cpp
+++ casion_number_game.cpp
@@ -1,16 +1,10 @@
-/******************************************************************************
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 /*------  CASION NUMBER GAME --------*/
-/*
-generate a random number. -------> \/
-get the total amount from user. ------>
-get the betting amount. ------- >
-let the user guess the number . ------->
+/* works:
+generate a random number. 
+get the total amount from user. 
+get the betting amount.
+let the user guess the number . 
 compare and change the betting money.
 */
 
